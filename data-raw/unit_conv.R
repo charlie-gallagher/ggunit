68679847840ab85a11c7b0938a4ed3e81e19bf37
Charlie Gallagher <charlie@ixisdigital.com>
2022-03-23 12:58:30 -0400

    Update package data to include inches

--- data-raw/unit_conv.R
+++ data-raw/unit_conv.R
@@ -5,6 +5,9 @@ picas <- pts / 12
 dida <- 1157 * pts / 1238
 cicero <- dida / 12
 
+dida_to_in <- 1157/1238/72.27
+cicero_to_in <- dida_to_in / 12
+
 unit_conv <- data.frame(
     unit = c("cm", "centimeter", "centimeters", "centimetre", "centimetres",
              "in", "inch", "inches",
@@ -16,6 +19,11 @@ unit_conv <- data.frame(
                    1, 1, 1, 1, 1,
                    pts, pts, pts, pts,
                    picas, bigpts, dida, cicero, pts/65536),
+    conv_to_in = c(2.54, 2.54, 2.54, 2.54, 2.54,
+                   1, 1, 1,
+                   25.4, 25.4, 25.4, 25.4, 25.4,
+                   1/72.27, 1/72.27, 1/72.27, 1/72.27,
+                   1/6, 1/72, dida_to_in, cicero_to_in, 1/65536),
     stringsAsFactors = FALSE
 )
 
